cc5440d6c8f3aff425807f21fdfa12f6456aab7a
Mike Anderson <mikea@candicontrols.com>
2021-04-20 16:22:03 -0700

    feat(Question): Implemented the question
    * Updated the mapStateToProps() to have all the needed data.
    
    * Stil
    lneeds the submit handled, but wanted to close the NAV feature.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,68 +1,107 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+//import { withRouter } from 'react-router-dom';
 
 class Question extends Component {
-    sFunc = 'Question'
+    sFunc = 'Question';
+
+    handleChange = () => {
+
+    }
+
     render() {
-        const sFunc = this.sFunc + '.render()-->'
+        const sFunc = this.sFunc + '.render()-->';
 
         console.log( sFunc + 'props', this.props );
 
-        const { id, questions, users /*authedUserId*/ } = this.props;
-        const thisQuestion = questions[id];
-        const questionsUser = users[thisQuestion.authorId];
+        const { question, author, authedUserChoice } = this.props;
 
         return (
             <span className="question-full">
                 <div className="question-asking">
-                    <h3>&nbsp;&nbsp;{questionsUser.name} asks:</h3>
+                    <h3>&nbsp;&nbsp;{author.name} asks:</h3>
                 </div>
                 <div className="question-block">
                     <div className="question-avatar">
                         <img
-                            src={questionsUser.avatarURL}
+                            src={author.avatarURL}
                             width="90"
-                            alt={'Image of ' + questionsUser.name}
+                            alt={'Image of ' + author.name}
                         />
                     </div>
                     <div className="question-details">
                         <form>
+                            <label>
                                 <input
-                                    type='radio'
-                                    value="questionOne"
-                                    name='questionChoice'
-                                >
-                                    thisQuestion.optionOne.text
-                                </input>
+                                    type="radio"
+                                    name="questionChoice"
+                                    value="optionOne"
+                                    checked={authedUserChoice === 'optionOne'}
+                                    onChange={this.handleChange}
+                                />
+                                {question.optionOne.text}
+                            </label>
+                            <br/>
+                            <label>
                                 <input
-                                    type='radio'
-                                    value="questionTwo"
-                                    name='questionChoice'
-                                >
-                                    thisQuestion.optionTwo.text
-                                </input>
+                                    type="radio"
+                                    name="questionChoice"
+                                    value="optionTwo"
+                                    checked={authedUserChoice === 'optionTwo'}
+                                    onChange={this.handleChange}
+                                />
+                                {question.optionTwo.text}
+                            </label>
+                            <br/>
                             <button>
                                 Submit
                             </button>
                         </form>
                     </div>
-    </div>
-    </span>
+                </div>
+            </span>
 
-    );
+        );
     }
 }
 
 Question.propTypes = {
-    id : PropTypes.string.isRequired,
+    id : PropTypes.string, //.isRequired,
 };
 
-function mapStateToProps( { questions, users, authedUserId } ) {
+function mapStateToProps( { questions, users, authedUser }, props ) {
+    const sFunc = 'Question.mapStateToProps()-->';
+    const debug = true;
+
+    const { id } = props.match.params;
+
+    if ( debug ) {
+        console.log( sFunc + 'props', props );
+        console.log( sFunc + 'questions', questions );
+        console.log( sFunc + 'users', users );
+        console.log( sFunc + 'authedUser', authedUser );
+        console.log( sFunc + 'id', id );
+    }
+
+    const question = questions[id];
+    const author = users[question.authorId];
+    let authedUserChoice = 'none';
+    let v = question.optionOne.votes.find( (v) => ( v === authedUser ) );
+    if ( v ) {
+        authedUserChoice = 'optionOne'
+    }
+    else if ( !authedUserChoice ) {
+        v = question.optionTwo.votes.find( (v) => ( v === authedUser ) );
+        if ( v ) authedUserChoice = 'optionTwo'
+    }
+
+    console.log( sFunc + 'authedUserChoice', authedUserChoice );
+
     return {
-        questions,
-        users,
-        authedUserId,
+        question,
+        author,
+        authedUserChoice,
     };
 }
 
