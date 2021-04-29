5448b1d495f202d15ab89f42744c93411609c9c4
Mike Anderson <mikea@candicontrols.com>
2021-04-29 15:16:07 -0700

    feat(authedUser): Don't understand this function.

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,6 +1,9 @@
 import { GLOBALS } from '../actions/shared';
 
+// I don't understand this reducer.  Why is state default to null here?  every other reducer it's {}
 export default function authedUser( state = null, action ) {
+    //const sFunc = 'reducers/authedUser()-->';
+
     switch ( action.type ) {
         case GLOBALS.USERS.LOGIN:
             return action.authedUserId;
