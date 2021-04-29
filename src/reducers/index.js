8c581317e00aa267845644e63b7d73079334958b
Mike Anderson <mikea@candicontrols.com>
2021-04-29 15:17:58 -0700

    feat(questionTab - index): added the questions to the inde.js for the reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authedUser from './authedUser';
 import users from './users';
 import questions from './questions';
+import questionsTab from './questionsTab';
 import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers( {
                                     authedUser,
                                     users,
-                                    questions : questions,
+                                    questions,
                                     loadingBar : loadingBarReducer,
+                                    questionsTab,
                                 } );
